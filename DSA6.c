c72446497d69a6f4bd7279cccc89220af796537f
krakesh80888-design <krakesh80888@gmail.com>
2026-04-13 08:46:11 +0530

    Add files via upload

---
+++ DSA6.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int arr[100],n,pos,element,i;
+printf("Enter number of elements:");
+scanf("%d",&n);
+printf("enter elements:\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+printf("enter position to insert (1 to %d):",n+1);
+scanf("%d",&pos);
+printf("enter element to insert:");
+scanf("%d",&element);
+for(i=n;i>=pos;i--)
+{
+arr[i]= arr[i-1];
+}
+arr[pos-1]= element;
+n++;
+printf("array after insertion:\n");
+for(i=0;i<n;i++)
+{
+for(i=0;i<n;i++)
+{
+printf("%d",arr[i]);
+}
+return 0;
+}
+}
